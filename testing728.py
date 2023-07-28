322982c4ee36a9941c60192eb5ec8f590289139a
Nilin Abrahamsen <nilin@berkeley.edu>
2023-07-28 16:13:35 -0700

    rename

--- testing728.py
+++ testing728.py
@@ -1,72 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Training a Simple Neural Network, with PyTorch Data Loading
-# 
-# [![Open in Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/google/jax/blob/main/docs/notebooks/Neural_Network_and_Data_Loading.ipynb) [![Open in Kaggle](https://kaggle.com/static/images/open-in-kaggle.svg)](https://kaggle.com/kernels/welcome?src=https://github.com/google/jax/blob/main/docs/notebooks/Neural_Network_and_Data_Loading.ipynb)
-# 
-# **Copyright 2018 The JAX Authors.**
-# 
-# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-# https://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# # New Section
-
-# ![JAX](https://raw.githubusercontent.com/google/jax/main/images/jax_logo_250px.png)
-# 
-# Let's combine everything we showed in the [quickstart notebook](https://colab.research.google.com/github/google/jax/blob/main/docs/notebooks/quickstart.ipynb) to train a simple neural network. We will first specify and train a simple MLP on MNIST using JAX for the computation. We will use PyTorch's data loading API to load images and labels (because it's pretty great, and the world doesn't need yet another data loading library).
-# 
-# Of course, you can use JAX with any API that is compatible with NumPy to make specifying the model a bit more plug-and-play. Here, just for explanatory purposes, we won't use any neural network libraries or special APIs for building our model.
-
-# In[ ]:
-
-
 import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax import random
+import jax
+from jax.scipy.special import logsumexp
+import numpy as np
+from torch.utils import data
+from torchvision.datasets import MNIST
+import time
+from util import *
 
-
-# ## Hyperparameters
-# Let's get a few bookkeeping items out of the way.
-
-# In[ ]:
-
-
-# A helper function to randomly initialize weights and biases
-# for a dense neural network layer
 def random_layer_params(m, n, key, scale=1e-2):
   w_key, b_key = random.split(key)
   return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
 
-# Initialize all layers for a fully-connected neural network with sizes "sizes"
 def init_network_params(sizes, key):
   keys = random.split(key, len(sizes))
   return [random_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
 
-layer_sizes = [784, 512, 512, 10]
-step_size = 0.01
+#layer_sizes = [784, 512, 512, 10]
+layer_sizes = [784, 128, 128, 10]
+step_size = 0.3
 num_epochs = 8
-batch_size = 128
+batch_size = 16
 n_targets = 10
 params = init_network_params(layer_sizes, random.PRNGKey(0))
-
 
 # ## Auto-batching predictions
 # 
 # Let us first define our prediction function. Note that we're defining this for a _single_ image example. We're going to use JAX's `vmap` function to automatically handle mini-batches, with no performance penalty.
 
-# In[ ]:
-
-
-from jax.scipy.special import logsumexp
 
 def relu(x):
   return jnp.maximum(0, x)
@@ -83,32 +45,7 @@ def predict(params, image):
   return logits - logsumexp(logits)
 
 
-# Let's check that our prediction function only works on single images.
-
-# In[ ]:
-
-
-# This works on single examples
-random_flattened_image = random.normal(random.PRNGKey(1), (28 * 28,))
-preds = predict(params, random_flattened_image)
-print(preds.shape)
-
-
-# In[ ]:
-
-
-# Doesn't work with a batch
 random_flattened_images = random.normal(random.PRNGKey(1), (10, 28 * 28))
-try:
-  preds = predict(params, random_flattened_images)
-except TypeError:
-  print('Invalid shapes!')
-
-
-# In[ ]:
-
-
-# Let's upgrade it to handle batches using `vmap`
 
 # Make a batched version of the `predict` function
 batched_predict = vmap(predict, in_axes=(None, 0))
@@ -116,13 +53,6 @@ batched_predict = vmap(predict, in_axes=(None, 0))
 # `batched_predict` has the same call signature as `predict`
 batched_preds = batched_predict(params, random_flattened_images)
 print(batched_preds.shape)
-
-
-# At this point, we have all the ingredients we need to define our neural network and train it. We've built an auto-batched version of `predict`, which we should be able to use in a loss function. We should be able to use `grad` to take the derivative of the loss with respect to the neural network parameters. Last, we should be able to use `jit` to speed up everything.
-
-# ## Utility and loss functions
-
-# In[ ]:
 
 
 def one_hot(x, k, dtype=jnp.float32):
@@ -138,29 +68,64 @@ def loss(params, images, targets):
   preds = batched_predict(params, images)
   return -jnp.mean(preds * targets)
 
-@jit
-def update(params, x, y):
-  grads = grad(loss)(params, x, y)
-  return [(w - step_size * dw, b - step_size * db)
-          for (w, b), (dw, db) in zip(params, grads)]
+
+def get_update_fn(gradfn=None):
+  if gradfn is None:
+    gradfn=lambda *args: jax.grad(loss)(*args),None
+
+  def update(params, x, y):
+    grads,aux = gradfn(params, x, y)
+    return [(w - step_size * dw, b - step_size * db)
+            for (w, b), (dw, db) in zip(params, grads)], aux
+  
+  return jax.jit(update)
+
+
+
+### new method ###
+
+jac=jax.jacrev(batched_predict)
+
+def newgradfn(params,images,targets):
+
+  O=jac(params,images)
+  O_=flattenjac(O,batchdims=2)
+  O_=jnp.reshape(O_,(-1,O_.shape[-1]))
+  T=jnp.inner(O_,O_)
+
+  e=batched_predict(params,images)-targets
+  e=jnp.ravel(e)
+
+  l,v=jnp.linalg.eigh(T)
+  valid=l>jnp.quantile(l,.2)
+  inv=(1/l)*valid
+
+  invT=v*inv[None,:] @ v.T
+  testT=v*l[None,:] @ v.T
+
+  xwise_grads=invT @ e
+
+  def contract(D):
+    D=jnp.reshape(D,(-1,)+D.shape[2:])
+    D=jnp.moveaxis(D,0,-1)
+    return D @ xwise_grads
+
+  return jax.tree_map(contract,O), e
+
+def flattenjac(T,batchdims):
+  flatten_array=lambda A:jnp.reshape(A,A.shape[:batchdims]+(-1,))
+  T=jax.tree_map(flatten_array,T)
+  T,_=jax.tree_flatten(T)
+  T=jnp.concatenate(T,axis=-1)
+  return T
+
+### end new method ###
+
+
 
 
 # ## Data Loading with PyTorch
-# 
-# JAX is laser-focused on program transformations and accelerator-backed NumPy, so we don't include data loading or munging in the JAX library. There are already a lot of great data loaders out there, so let's just use them instead of reinventing anything. We'll grab PyTorch's data loader, and make a tiny shim to make it work with NumPy arrays.
 
-# In[ ]:
-
-
-#get_ipython().system('pip install torch torchvision')
-
-
-# In[ ]:
-
-
-import numpy as np
-from torch.utils import data
-from torchvision.datasets import MNIST
 
 def numpy_collate(batch):
   if isinstance(batch[0], np.ndarray):
@@ -194,16 +159,9 @@ class FlattenAndCast(object):
     return np.ravel(np.array(pic, dtype=jnp.float32))
 
 
-# In[ ]:
-
-
 # Define our dataset, using torch datasets
 mnist_dataset = MNIST('/tmp/mnist/', download=True, transform=FlattenAndCast())
 training_generator = NumpyLoader(mnist_dataset, batch_size=batch_size, num_workers=0)
-
-
-# In[ ]:
-
 
 # Get the full train dataset (for checking accuracy while training)
 train_images = np.array(mnist_dataset.train_data).reshape(len(mnist_dataset.train_data), -1)
@@ -217,16 +175,15 @@ test_labels = one_hot(np.array(mnist_dataset_test.test_labels), n_targets)
 
 # ## Training Loop
 
-# In[ ]:
-
-
-import time
+update=get_update_fn(newgradfn)
+#update=get_update_fn()
 
 for epoch in range(num_epochs):
   start_time = time.time()
-  for x, y in training_generator:
+  for i, (x, y) in enumerate(training_generator):
     y = one_hot(y, n_targets)
-    params = update(params, x, y)
+    params, aux = update(params, x, y)
+    print(jnp.linalg.norm(aux))
   epoch_time = time.time() - start_time
 
   train_acc = accuracy(params, train_images, train_labels)
--- util.py
+++ util.py
@@ -1,15 +1,18 @@
 def tostring(T, nonestring=''):
   notleaf=lambda T: isinstance(T,list) or isinstance(T,tuple)
-  indent=lambda block:'\n'.join(['| '+l for l in block.splitlines()])
+  indent=lambda block:'\n'.join([u"\u2588"+l for l in block.splitlines()])
   if notleaf(T):
-    return 'O'+'\n'+'\n'.join([indent(tostring(t)) for t in T])
+    return '\n\n'.join([indent(tostring(t)) for t in T])
   else:
     if T is None:
       return nonestring
     else:
       return str(T.shape)
+    
+def ts(T):
+  print(tostring(T))
 
-def testtostring(): 
+if __name__=='__main__':
   import jax.numpy as jnp
   A=jnp.ones((10,5))
   print(tostring([((A,),A),(A,)]))
